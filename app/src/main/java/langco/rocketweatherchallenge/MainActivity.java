2a9158da3b0d4e2e7694e9544f8028a2ad4aafb8
langbenj <langbenj@gmail.com>
2016-04-19 09:38:43 -0500

    Initial commit. Weather JSON read and viewpager implemented

---
+++ app/src/main/java/langco/rocketweatherchallenge/MainActivity.java
@@ -0,0 +1,162 @@
+package langco.rocketweatherchallenge;
+
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String default_url_to_pass="http://forecast.weather.gov/MapClick.php?lat=41.885575&lon=-87.644408&FcstType=json";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        SectionsPagerAdapter  mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        new WeatherJSONReader().execute(default_url_to_pass);
+
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public static class PlaceholderFragment extends Fragment {
+
+        //Variable that specifies the view page you are currently on.
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.display);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+
+        public String[] getTemperature() throws IOException {
+
+
+            return null;
+
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 5;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return return_date(0,"MM/dd/yy");
+                case 1:
+                    return return_date(1,"MM/dd/yy");
+                case 2:
+                    return return_date(2,"MM/dd/yy");
+                case 3:
+                    return return_date(3,"MM/dd/yy");
+                case 4:
+                    return return_date(4,"MM/dd/yy");
+            }
+            return null;
+        }
+
+        public String return_date (int offset, String format) {
+            Calendar todays_date = Calendar.getInstance();
+            todays_date.add(Calendar.DATE,offset);
+            SimpleDateFormat formatted_date=new SimpleDateFormat(format);
+            return formatted_date.format(todays_date.getTime());
+        }
+    }
+}
---
+++ app/src/main/java/langco/rocketweatherchallenge/WeatherJSONReader.java
@@ -0,0 +1,118 @@
+package langco.rocketweatherchallenge;
+
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+
+//Data is read in an AsyncTask thread to prevent freezes and other errors.
+public class WeatherJSONReader extends AsyncTask<String,Integer,ArrayList<String>> {
+
+    @Override
+    public ArrayList<String> doInBackground(String... urls) {
+        //Initialize the return value, this will be built as the data is read
+        String return_value ="";
+
+        //We'll only be using 1 URL but this piece of code could be modified to read multiple URLs.
+        for (String url : urls) {
+            URL built_url;
+            try {
+                built_url = new URL(url);
+                try {
+                    //Open the connection to the site specified by the URL passed into the class
+                    HttpURLConnection http_connection = (HttpURLConnection) built_url.openConnection();
+                    try {
+                        //As the data comes in through the InputStream it is converted into a readable String format
+                        //through use of BufferedReader as each line comes in it's appended to the return value
+                        InputStream input = new BufferedInputStream(http_connection.getInputStream());
+                        BufferedReader buffer_stream = new BufferedReader(new InputStreamReader(input));
+                        String buffer_line;
+                        while ((buffer_line = buffer_stream.readLine()) != null) {
+                            return_value += buffer_line;
+                        }
+                    }
+                    finally {
+                        //When the read is complete disconnect the http connection to clean up memory and stop conflicts
+                        http_connection.disconnect();
+                    }
+                } catch (IOException e) {
+                    //Print the stack trace for debugging if there's an IO exception
+                    e.printStackTrace();
+                }
+            } catch (MalformedURLException e) {
+                //If the URL is not formed correctly print out the stack trace for debugging
+                e.printStackTrace();
+            }
+        }
+
+        //Parse the output from the url
+        ArrayList<String> temperatures= new ArrayList<>();
+        ArrayList<String> descriptions= new ArrayList<>();
+        String start_period="Tonight";
+        try {
+            JSONObject full_json_object = new JSONObject(return_value);
+            //Generate data arrays for temperatures and descriptions
+            JSONObject data_object = full_json_object.getJSONObject("data");
+            JSONArray temperature_array = data_object.getJSONArray("temperature");
+            JSONArray description_array = data_object.getJSONArray("text");
+            for (int i=0; i<temperature_array.length(); i++) {
+                temperatures.add(temperature_array.getString(i));
+            }
+            for (int i=0; i<description_array.length(); i++) {
+                descriptions.add(description_array.getString(i));
+            }
+
+            //Grab the first value in the Start Period Name. This is used to determine if the format
+            //is for the day or night
+            JSONObject time_object = full_json_object.getJSONObject("time");
+            JSONArray day_array = time_object.getJSONArray("startPeriodName");
+            start_period = day_array.getString(0);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // The feed changes based on the time of day. If it's between 5:00 AM and 6:00 PM the
+        // first value in the returned data is today. Otherwise it's tonight. This throws off
+        // all of the calculations unless I check for it. Sneaky.
+
+        //This is where we generate the strings that will be used to fill the views in the viewpager
+        ArrayList<String> result_list= new ArrayList<>();
+        int offset;
+        if (start_period.equals("Tonight")) {
+            offset = 1;
+        }
+        else {
+            offset = 0;
+        }
+        for (int x = 0; x < 5; x++) {
+            result_list.add("High: " + temperatures.get((x * 2) + offset) + "\n" +
+                    "Description: " + descriptions.get((x * 2) + offset));
+            }
+
+        //Return the results
+        return result_list;
+    }
+
+
+    protected void onPostExecute(ArrayList<String> result) {
+        int x=0;
+
+    }
+}
